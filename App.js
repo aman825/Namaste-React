47ca5e8c096844678e8fe1442f45282a42790698
aman825 <amanambar@gmail.com>
2023-12-25 00:07:18 +0530

    Till end of Ep 2

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
 // const heading = React.createElement("h1", {id :"heading"}, "Hello World from React");
 let e = React.createElement;
 const parent = e("div", 
-{id :"parent"}, [e("div", {id:"child"}, [e("h1", "", "I am an h1"), e("h2", "", "I am an h2")]), e("div", {id:"child2"}, [e("h1", "", "I am an h1"), e("h2", "", "I am an h2")] )]
+{id :"parent"}, [e("div", {id:"child"}, [e("h1", "", "I am an h10"), e("h2", "", "I am an h2")]), e("div", {id:"child2"}, [e("h1", "", "I am an h1"), e("h2", "", "I am an h2")] )]
 );
 
 
